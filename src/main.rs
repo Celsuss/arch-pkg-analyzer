98990714932003fa523e7c964b2ac73ba2877023
Celsuss <jenslorden@proton.me>
2026-01-29 22:28:45 +0100

    Added clap cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // Name of path to analye
+    #[arg(short, long)]
+    check_db: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello {}!", args.check_db);
 }
